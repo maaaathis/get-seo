88ca0208208aca1bcab1a1002d0e30198ba02782
Mathis <mathis.fullriede@gmail.com>
2023-12-27 21:35:54 +0100

    add prettier

--- src/getSEO.ts
+++ src/getSEO.ts
@@ -2,49 +2,51 @@ import { JSDOM } from 'jsdom';
 import fetch from 'node-fetch';
 
 export class getSEO {
-    private readonly url: string;
-    private html: string | null = null;
+  private readonly url: string;
+  private html: string | null = null;
 
-    constructor(url: string) {
-        this.url = url;
+  constructor(url: string) {
+    this.url = url;
+  }
+
+  private async fetchHtml(): Promise<void> {
+    try {
+      const response = await fetch(this.url);
+      this.html = await response.text();
+    } catch (error) {
+      console.error('Error fetching HTML:', error);
+      throw error;
+    }
+  }
+
+  private parseHtml(): Document {
+    if (!this.html) {
+      throw new Error('HTML not fetched. Call fetchHtml() before parsing.');
+    }
+    const dom = new JSDOM(this.html);
+    return dom.window.document;
+  }
+
+  async getTitle(): Promise<string> {
+    if (!this.html) {
+      await this.fetchHtml();
     }
 
-    private async fetchHtml(): Promise<void> {
-        try {
-            const response = await fetch(this.url);
-            this.html = await response.text();
-        } catch (error) {
-            console.error('Error fetching HTML:', error);
-            throw error;
-        }
+    const document = this.parseHtml();
+    const title = document.querySelector('head title')?.textContent || '';
+    return title;
+  }
+
+  async getDescription(): Promise<string> {
+    if (!this.html) {
+      await this.fetchHtml();
     }
 
-    private parseHtml(): Document {
-        if (!this.html) {
-            throw new Error('HTML not fetched. Call fetchHtml() before parsing.');
-        }
-        const dom = new JSDOM(this.html);
-        return dom.window.document;
-    }
-
-    async getTitle(): Promise<string> {
-        if (!this.html) {
-            await this.fetchHtml();
-        }
-
-        const document = this.parseHtml();
-        const title = document.querySelector('head title')?.textContent || '';
-        return title;
-    }
-
-    async getDescription(): Promise<string> {
-        if (!this.html) {
-            await this.fetchHtml();
-        }
-
-        const document = this.parseHtml();
-        const description =
-            document.querySelector('meta[name="description"]')?.getAttribute('content') || '';
-        return description;
-    }
+    const document = this.parseHtml();
+    const description =
+      document
+        .querySelector('meta[name="description"]')
+        ?.getAttribute('content') || '';
+    return description;
+  }
 }
--- test.js
+++ test.js
@@ -1,16 +1,19 @@
 import test from 'ava';
-import { getSEO } from "./dist/getSEO.js"
+import { getSEO } from './dist/getSEO.js';
 
 const TEST_URL = 'https://github.com/maaaathis/maaaathis';
 
-test('title', async t => {
-    const seoGetter = new getSEO(TEST_URL);
-    const title = await seoGetter.getTitle();
-    t.is(title, 'GitHub - maaaathis/maaaathis: Personal GitHub Profile');
+test('title', async (t) => {
+  const seoGetter = new getSEO(TEST_URL);
+  const title = await seoGetter.getTitle();
+  t.is(title, 'GitHub - maaaathis/maaaathis: Personal GitHub Profile');
 });
 
-test('description', async t => {
-    const seoGetter = new getSEO(TEST_URL);
-    const description = await seoGetter.getDescription();
-    t.is(description, 'Personal GitHub Profile. Contribute to maaaathis/maaaathis development by creating an account on GitHub.');
+test('description', async (t) => {
+  const seoGetter = new getSEO(TEST_URL);
+  const description = await seoGetter.getDescription();
+  t.is(
+    description,
+    'Personal GitHub Profile. Contribute to maaaathis/maaaathis development by creating an account on GitHub.'
+  );
 });
